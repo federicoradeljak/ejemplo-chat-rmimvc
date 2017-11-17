108a1118b4d0544e49a59ad68de847837aec344e
federico.radeljak <federico.radeljak@gmail.com>
2017-11-17 18:29:02 -0300

    ahora el cliente y el servidor piden el ingreso de la IP y del puerto

--- src/ar/edu/unlu/chatmvc/cliente/AppCliente.java
+++ src/ar/edu/unlu/chatmvc/cliente/AppCliente.java
@@ -4,18 +4,56 @@ import java.rmi.AccessException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 import ar.edu.unlu.chatmvc.controlador.Controlador;
 import ar.edu.unlu.chatmvc.vista.IVista;
 import ar.edu.unlu.chatmvc.vista.grafica.VistaGrafica;
+import ar.edu.unlu.mvcrmi.Util;
 import ar.edu.unlu.mvcrmi.cliente.Cliente;
 
 public class AppCliente {
 
 	public static void main(String[] args) {
+		ArrayList<String> ips = Util.getIpDisponibles();
+		String ip = (String) JOptionPane.showInputDialog(
+				null, 
+				"Seleccione la IP en la que escuchará peticiones el cliente", "IP del cliente", 
+				JOptionPane.QUESTION_MESSAGE, 
+				null,
+				ips.toArray(),
+				null
+		);
+		String port = (String) JOptionPane.showInputDialog(
+				null, 
+				"Seleccione el puerto en el que escuchará peticiones el cliente", "Puerto del cliente", 
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				null,
+				9999
+		);
+		String ipServidor = (String) JOptionPane.showInputDialog(
+				null, 
+				"Seleccione la IP en la corre el servidor", "IP del servidor", 
+				JOptionPane.QUESTION_MESSAGE, 
+				null,
+				null,
+				null
+		);
+		String portServidor = (String) JOptionPane.showInputDialog(
+				null, 
+				"Seleccione el puerto en el que corre el servidor", "Puerto del servidor", 
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				null,
+				8888
+		);
 		Controlador controlador = new Controlador();
 		IVista vista = new VistaGrafica(controlador);
-		Cliente c = new Cliente("127.0.0.1", 8888, "127.0.0.1", 9999);
+		Cliente c = new Cliente(ip, Integer.parseInt(port), ipServidor, Integer.parseInt(portServidor));
+		vista.iniciar();
 		try {
 			c.iniciar(controlador);
 		} catch (AccessException e) {
--- src/ar/edu/unlu/chatmvc/servidor/AppServidor.java
+++ src/ar/edu/unlu/chatmvc/servidor/AppServidor.java
@@ -1,16 +1,38 @@
 package ar.edu.unlu.chatmvc.servidor;
 
+import java.net.InetAddress;
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 import ar.edu.unlu.chatmvc.modelo.Chat;
+import ar.edu.unlu.mvcrmi.Util;
 import ar.edu.unlu.mvcrmi.servidor.Servidor;
 
 public class AppServidor {
 
 	public static void main(String[] args) {
+		ArrayList<String> ips = Util.getIpDisponibles();
+		String ip = (String) JOptionPane.showInputDialog(
+				null, 
+				"Seleccione la IP en la que escuchará peticiones el servidor", "IP del servidor", 
+				JOptionPane.QUESTION_MESSAGE, 
+				null,
+				ips.toArray(),
+				null
+		);
+		String port = (String) JOptionPane.showInputDialog(
+				null, 
+				"Seleccione el puerto en el que escuchará peticiones el servidor", "Puerto del servidor", 
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				null,
+				8888
+		);
 		Chat modelo = new Chat();
-		Servidor servidor = new Servidor("192.168.0.16", 8888);
+		Servidor servidor = new Servidor(ip, Integer.parseInt(port));
 		try {
 			servidor.iniciar(modelo);
 		} catch (RemoteException e) {
